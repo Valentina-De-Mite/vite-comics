cebe891a0b3b47bbb3d9a6d79f7132cb76f07613
Valentina De Mite <valesakura97@gmail.com>
2023-09-22 22:55:22 +0200

    added active bordo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -12,19 +12,28 @@ export default {
   <div class="container">
     <nav class="navbar navbar-expand justify-content-between">
       <AppLogo></AppLogo>
-      <div class="nav navbar-nav py-5">
-        <a class="nav-item nav-link" href="#" aria-current="page">CHARACTERS</a>
-        <a class="nav-item nav-link" href="#">COMICS</a>
-        <a class="nav-item nav-link active-item" href="#">MOVIES</a>
-        <a class="nav-item nav-link" href="#">TV</a>
-        <a class="nav-item nav-link" href="#">COLLECTIBLES</a>
-        <a class="nav-item nav-link" href="#">VIDEOS</a>
-        <a class="nav-item nav-link" href="#">FANS</a>
-        <a class="nav-item nav-link" href="#">NEWS</a>
-        <a class="nav-item nav-link" href="#">SHOP</a>
+      <div class="nav navbar-nav h-100">
+        <a class="nav-item nav-link bordo" href="#" aria-current="page"
+          >CHARACTERS</a
+        >
+        <a class="nav-item nav-link bordo" href="#">COMICS</a>
+        <a class="nav-item nav-link bordo" href="#">MOVIES</a>
+        <a class="nav-item nav-link bordo" href="#">TV</a>
+        <a class="nav-item nav-link bordo" href="#">COLLECTIBLES</a>
+        <a class="nav-item nav-link bordo" href="#">VIDEOS</a>
+        <a class="nav-item nav-link bordo" href="#">FANS</a>
+        <a class="nav-item nav-link bordo" href="#">NEWS</a>
+        <a class="nav-item nav-link bordo" href="#">SHOP</a>
       </div>
     </nav>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.bordo:hover {
+  border-bottom: 5px solid rgb(38, 77, 207) !important;
+}
+a {
+  height: 100%;
+}
+</style>
