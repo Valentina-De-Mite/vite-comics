070b0905ab319a69c2b572d46298593cec144a39
Valentina De Mite <valesakura97@gmail.com>
2023-09-22 14:38:20 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import AppFooter from "./components/AppFooter.vue";
+</script>
+
+<template></template>
+
+<style></style>
