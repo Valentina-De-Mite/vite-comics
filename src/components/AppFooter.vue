b0ffb3c65bc9e0684b8f781cb37931a0f20d4236
Valentina De Mite <valesakura97@gmail.com>
2023-09-22 22:22:18 +0200

    added footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,5 +1,119 @@
-<script></script>
+<script>
+export default {
+  name: "AppFooter",
+};
+</script>
 
-<template></template>
+<template>
+  <div class="jumbotron text-light py-5">
+    <div class="container row justify-content-center m-auto">
+      <div class="col-6 d-flex">
+        <div class="col-4 f-s d-flex flex-column">
+          <h5 class="link pb-1">DC COMICS</h5>
+          <a class="text-decoration-none link pb-3">Characters</a>
+          <a class="text-decoration-none link pb-3">Comics</a>
+          <a class="text-decoration-none link pb-3">Movies</a>
+          <a class="text-decoration-none link pb-3">TV</a>
+          <a class="text-decoration-none link pb-3">Games</a>
+          <a class="text-decoration-none link pb-3">Videos</a>
+          <a class="text-decoration-none link pb-5">News</a>
+          <h5 class="pb-1 link">SHOP</h5>
+          <a class="text-decoration-none link pb-3">Shop DC</a>
+          <a class="text-decoration-none link pb-3">Shop DC Collectibles</a>
+        </div>
+        <div class="col-4 f-s d-flex flex-column">
+          <h5 class="link pb-1">DC</h5>
+          <a class="text-decoration-none link pb-3">Tearms Of Use</a>
+          <a class="text-decoration-none link pb-3">Privacy policy (New)</a>
+          <a class="text-decoration-none link pb-3">Ad Choices</a>
+          <a class="text-decoration-none link pb-3">Advertising</a>
+          <a class="text-decoration-none link pb-3">Jobs</a>
+          <a class="text-decoration-none link pb-3">Subscriptions</a>
+          <a class="text-decoration-none link pb-3">Talent Workshops</a>
+          <a class="text-decoration-none link pb-3">CPSC Certificates</a>
+          <a class="text-decoration-none link pb-3">Ratings</a>
+          <a class="text-decoration-none link pb-3">Shop Help</a>
+          <a class="text-decoration-none link pb-3">Contact Us</a>
+        </div>
+        <div class="col-4 f-s d-flex flex-column">
+          <h5 class="link pb-1">SITES</h5>
+          <a class="text-decoration-none link pb-3">DC</a>
+          <a class="text-decoration-none link pb-3">MAD Megazine</a>
+          <a class="text-decoration-none link pb-3">DC Kids</a>
+          <a class="text-decoration-none link pb-3">DC Universe</a>
+          <a class="text-decoration-none link pb-3">DC Power Visa</a>
+        </div>
+      </div>
+      <div class="col-6 DC d-flex">
+        <img
+          class="w-150 ms-5"
+          src="../assets/img/dc-logo-bg.png"
+          alt=""
+          srcset=""
+        />
+      </div>
+    </div>
+  </div>
 
-<style></style>
+  <div class="footer text-light py-5 dark">
+    <div class="container row justify-content-between m-auto">
+      <div class="col-2">
+        <button class="btn btn-outline-primary py-2">SING UP NOW!</button>
+      </div>
+      <div class="col-4 d-flex align-items-end">
+        <h5 class="tb text-primary me-4">FOLLOW US</h5>
+        <img
+          class="me-2"
+          src="../assets/img/footer-facebook.png"
+          alt=""
+          srcset=""
+        />
+        <img
+          class="me-2"
+          src="../assets/img/footer-twitter.png"
+          alt=""
+          srcset=""
+        />
+        <img
+          class="me-2"
+          src="../assets/img/footer-youtube.png"
+          alt=""
+          srcset=""
+        />
+        <img
+          class="me-2"
+          src="../assets/img/footer-pinterest.png"
+          alt=""
+          srcset=""
+        />
+        <img src="../assets/img/footer-periscope.png" alt="" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.jumbotron {
+  background-image: url(../assets/img/footer-bg.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.DC {
+  background-repeat: no-repeat;
+}
+.f-s {
+  font-size: 15px;
+}
+
+.dark {
+  background-color: #303030;
+}
+
+a {
+  color: white !important;
+}
+.link:hover,
+.tb:hover {
+  text-decoration: underline !important;
+}
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -84,7 +84,7 @@ export default {
   </div>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .black {
   background-color: black;
   color: white;
