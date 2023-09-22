518474d2557d7f4f6b527513ed18f3c412d18af3
Valentina De Mite <valesakura97@gmail.com>
2023-09-22 20:11:39 +0200

    added banner

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,20 +9,22 @@ export default {
 </script>
 
 <template>
-  <nav class="navbar navbar-expand justify-content-evenly">
-    <AppLogo></AppLogo>
-    <div class="nav navbar-nav">
-      <a class="nav-item nav-link" href="#" aria-current="page">CHARACTERS</a>
-      <a class="nav-item nav-link" href="#">COMICS</a>
-      <a class="nav-item nav-link active-item" href="#">MOVIES</a>
-      <a class="nav-item nav-link" href="#">TV</a>
-      <a class="nav-item nav-link" href="#">COLLECTIBLES</a>
-      <a class="nav-item nav-link" href="#">VIDEOS</a>
-      <a class="nav-item nav-link" href="#">FANS</a>
-      <a class="nav-item nav-link" href="#">NEWS</a>
-      <a class="nav-item nav-link" href="#">SHOP</a>
-    </div>
-  </nav>
+  <div class="container">
+    <nav class="navbar navbar-expand justify-content-between">
+      <AppLogo></AppLogo>
+      <div class="nav navbar-nav py-5">
+        <a class="nav-item nav-link" href="#" aria-current="page">CHARACTERS</a>
+        <a class="nav-item nav-link" href="#">COMICS</a>
+        <a class="nav-item nav-link active-item" href="#">MOVIES</a>
+        <a class="nav-item nav-link" href="#">TV</a>
+        <a class="nav-item nav-link" href="#">COLLECTIBLES</a>
+        <a class="nav-item nav-link" href="#">VIDEOS</a>
+        <a class="nav-item nav-link" href="#">FANS</a>
+        <a class="nav-item nav-link" href="#">NEWS</a>
+        <a class="nav-item nav-link" href="#">SHOP</a>
+      </div>
+    </nav>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,5 +1,95 @@
-<script></script>
+<script>
+export default {
+  name: "AppMain",
+};
+</script>
 
-<template></template>
+<template>
+  <div class="black py-5">
+    <div class="container">
+      <h5>Content Goes here</h5>
+    </div>
+  </div>
 
-<style></style>
+  <div class="blue fs-4 text-light py-3">
+    <div
+      class="container row m-auto justify-content-between align-items-center"
+    >
+      <div class="col-2 d-flex justify-content-center">
+        <div
+          class="text-center justify-content-center d-flex align-items-center"
+        >
+          <img
+            class="w-25"
+            src="../assets/img/buy-comics-digital-comics.png"
+            alt=""
+            srcset=""
+          />
+          <p class="ms-2 fs-6">DIGITAL COMICS</p>
+        </div>
+      </div>
+      <div class="col-2 d-flex justify-content-center">
+        <div
+          class="text-center justify-content-center d-flex align-items-center"
+        >
+          <img
+            class="w-25"
+            src="../assets/img/buy-comics-merchandise.png"
+            alt=""
+            srcset=""
+          />
+          <p class="ms-2 fs-6">DC MERCHANDISE</p>
+        </div>
+      </div>
+      <div class="col-2 d-flex justify-content-center">
+        <div
+          class="text-center justify-content-center d-flex align-items-center"
+        >
+          <img
+            class="w-25"
+            src="../assets/img/buy-comics-subscriptions.png"
+            alt=""
+            srcset=""
+          />
+          <p class="ms-2 fs-6">SUBSCRITION</p>
+        </div>
+      </div>
+      <div class="col-2 d-flex justify-content-center">
+        <div
+          class="text-center justify-content-center d-flex align-items-center"
+        >
+          <img
+            class="w-25"
+            src="../assets/img/buy-comics-shop-locator.png"
+            alt=""
+            srcset=""
+          />
+          <p class="ms-2 fs-6">COMIC SHOP LOCATOR</p>
+        </div>
+      </div>
+      <div class="col-2 d-flex justify-content-center">
+        <div
+          class="text-center justify-content-center d-flex align-items-center"
+        >
+          <img
+            class="w-25"
+            src="../assets/img/buy-comics-digital-comics.png"
+            alt=""
+            srcset=""
+          />
+          <p class="ms-2 fs-6">DC POWER VISA</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.black {
+  background-color: black;
+  color: white;
+}
+.blue {
+  background-color: #68bafc;
+}
+</style>
